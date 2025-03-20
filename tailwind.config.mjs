561c2698ea6cf0f453ac65509e6f4a7275570fe0
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2025-03-20 13:28:22 +0100

    Added borderbox to fix gap between hero and categoryList

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 // tailwind.config.js
 module.exports = {
     content: [
-      './src/app/**/*.{js,ts,jsx,tsx}', // For Next.js 13+ app directory
+      './src/app/**/*.{js,ts,jsx,tsx,css}', // For Next.js 13+ app directory
       './src/components/**/*.{js,ts,jsx,tsx}', // For components directory
     ],
     theme: {
