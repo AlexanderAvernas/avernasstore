3d6016e0817feb90d617d8159df1cb5125776913
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2025-12-07 13:25:35 +0100

    Uppdated next.js, netlify plugings and tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,32 @@
-// tailwind.config.js
-module.exports = {
-    content: [
-      './src/app/**/*.{js,ts,jsx,tsx,css}', // For Next.js 13+ app directory
-      './src/components/**/*.{js,ts,jsx,tsx}', // For components directory
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
+// // tailwind.config.js
+// module.exports = {
+//     content: [
+//       './src/app/**/*.{js,ts,jsx,tsx,css}', // For Next.js 13+ app directory
+//       './src/components/**/*.{js,ts,jsx,tsx}', // For components directory
+//     ],
+//     theme: {
+//       extend: {
+//         fontFamily: {
+//         sans: ['var(--font-ibm-plex-sans)', 'system-ui', 'sans-serif'],
+//       },
+//       },
+//     },
+//     plugins: [],
+//   };
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,css}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
         sans: ['var(--font-ibm-plex-sans)', 'system-ui', 'sans-serif'],
       },
-      },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+}
